7dd486f340b2a5c69426c07178a697200dcd4a82
AmanuelTesfamichael <84349270+AmanuelTesfamichael@users.noreply.github.com>
2021-10-10 08:40:23 -0500

    first commit

---
+++ payment-service/src/main/java/com/javatechie/paymentservice/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.javatechie.paymentservice.repository;
+
+import com.javatechie.paymentservice.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+}
